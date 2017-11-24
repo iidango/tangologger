d7ee27f9d0c6ca5384e409707716432b4185e274
iida <iida@ucl.nuee.nagoya-u.ac.jp>
2017-11-23 17:03:32 -0800

    modify run shell scripts

--- manual_alignment/test/io/sh/tasc1_8000_e.sh
+++ manual_alignment/test/io/sh/tasc1_8000_e.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-PREFIX=/local-scratch/iida/project/navimap/floorplan/data/sfu/tasc1_8000_e
+# PREFIX=/local-scratch/iida/project/navimap/floorplan/data/sfu/tasc1_8000_c
+PREFIX=$1
 
 rm $PREFIX/images/*
 rm $PREFIX/images_northup/*
 python ../test_load_tangoPose.py $PREFIX $PREFIX $PREFIX/R0010228_er.MP4 3 0.3 -t
-python ../test_set_floorplan.py $PREFIX $PREFIX $PREFIX/floorplans/TASC1_8000Level.png 0 0 0.11 24 -13.5 1.8 4350 2625
+python ../test_set_floorplan.py $PREFIX $PREFIX $PREFIX/floorplans/tasc1_8000.png 0 0 0.11 24 -13.5 1.8
 python ../test_adjust_img_direction.py $PREFIX $PREFIX
